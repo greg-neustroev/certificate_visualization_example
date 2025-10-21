dfdd8ed97c1a428d3a678af77b437abfaa083720
Greg Neustroev <G.Neustroev@tudelft.nl>
2025-10-21 17:12:42 +0200

    Initial commit

---
+++ controlled_sde/__init__.py
@@ -0,0 +1,5 @@
+"""Provides classes for controlled stochastic differential equations"""
+from .controlled_sde import ControlledSDE
+from .inverted_pendulum import InvertedPendulum
+
+__all__ = ["ControlledSDE", "InvertedPendulum"]
